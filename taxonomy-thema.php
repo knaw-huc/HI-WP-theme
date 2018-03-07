f219d0fd56ae777e72f7dbd61b1b12079c1b6322
Pieter de Jong <contact@pieter-de-jong.nl>
2018-03-07 13:57:57 +0100

    Implement thema detail page

---
+++ taxonomy-thema.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+  <div class="page-heading">
+    <a class="page-heading__button" href="<?= get_post_type_archive_link('project'); ?>">Terug naar het overzicht</a>
+  </div>
+  <!-- end .page-heading -->
+
+  <div class="main main--white">
+
+    <main class="main__column main__column--body text-holder">
+
+      <h1><?php single_term_title(); ?></h1>
+
+      <?php the_field('thema_long_description'); ?>
+
+    </main>
+    <!-- end .main__column -->
+
+    <aside class="main__column main__column--aside main__column--empty sidebar">
+
+    </aside>
+    <!-- end .main__column -->
+
+  </div>
+  <!-- end .main -->
+
+<?php get_footer(); ?>
