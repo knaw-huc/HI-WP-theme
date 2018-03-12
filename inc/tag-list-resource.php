230a256418ac39cd326521dbd938493114e20691
Pieter de Jong <contact@pieter-de-jong.nl>
2018-03-12 11:22:36 +0100

    Update tag list

--- inc/tag-list-resource.php
+++ inc/tag-list-resource.php
@@ -3,34 +3,38 @@
 
 <?php if ($types || $posttags) { ?>
 
-  <div class="tag-list">
+  <object>
 
-    <?php if ($types) { ?>
+    <div class="tag-list">
 
-      <?php foreach($types as $type) { ?>
-        <!-- TODO: This should link to a pre-filtered resource overview -->
-        <div class="tag-list__item tag-list__item--type">Vakliteratuur</div>
-      <?php } ?>
+      <?php if ($types) { ?>
 
-    <?php } ?>
-
-    <?php if ($posttags) { ?>
-
-      <?php $i = 0; ?>
-
-      <?php foreach($posttags as $tag) { ?>
-
-        <?php if($i < 2) { ?>
+        <?php foreach($types as $type) { ?>
           <!-- TODO: This should link to a pre-filtered resource overview -->
-          <div class="tag-list__item"><?php echo $tag->name . ' '; ?></div>
-          <?php $i++; ?>
+          <div class="tag-list__item tag-list__item--type">Vakliteratuur</div>
         <?php } ?>
 
       <?php } ?>
 
-    <?php } ?>
+      <?php if ($posttags) { ?>
 
-  </div>
-  <!-- end .tag-list -->
+        <?php $i = 0; ?>
+
+        <?php foreach($posttags as $tag) { ?>
+
+          <?php if($i < 2) { ?>
+            <!-- TODO: This should link to a pre-filtered resource overview -->
+            <div class="tag-list__item"><?php echo $tag->name . ' '; ?></div>
+            <?php $i++; ?>
+          <?php } ?>
+
+        <?php } ?>
+
+      <?php } ?>
+
+    </div>
+    <!-- end .tag-list -->
+
+  </object>
 
 <?php } ?>
--- inc/tag-list.php
+++ inc/tag-list.php
@@ -2,14 +2,27 @@
 
 <?php if ($posttags) { ?>
 
-  <div class="tag-list">
+  <?php $i = 0; ?>
 
-    <?php foreach($posttags as $tag) { ?>
-      <!-- TODO: This should link to a pre-filtered project overview -->
-      <div class="tag-list__item"><?php echo $tag->name . ' '; ?></div>
-    <?php } ?>
+  <object>
 
-  </div>
-  <!-- end .tag-list -->
+    <div class="tag-list">
+
+      <?php foreach($posttags as $tag) { ?>
+
+        <?php if($i < 3) { ?>
+
+          <a href="<?= get_post_type_archive_link('project'); ?>?tag=<?= $tag->slug; ?>" class="tag-list__item"><?php echo $tag->name . ' '; ?></a>
+
+          <?php $i++; ?>
+
+        <?php } ?>
+
+      <?php } ?>
+
+    </div>
+    <!-- end .tag-list -->
+
+  </object>
 
 <?php } ?>
