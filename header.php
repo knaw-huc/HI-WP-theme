300f0c74e1221df34429c1d16bb52ffe895ab43d
Pieter de Jong <contact@pieter-de-jong.nl>
2018-05-22 20:08:20 +0200

    Add icons

--- header.php
+++ header.php
@@ -18,6 +18,15 @@
     <!-- CSS -->
     <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" />
 
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#002048">
+    <meta name="msapplication-TileColor" content="#002048">
+    <meta name="theme-color" content="#002048">
+
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
