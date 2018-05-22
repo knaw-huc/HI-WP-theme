b5b78a7484045f899772b536a9e1b1e7d8db1ec9
Pieter de Jong <contact@pieter-de-jong.nl>
2018-05-22 20:08:13 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+  <div class="page-heading">
+    <a class="page-heading__button" href="<?php bloginfo('url'); ?>">Terug naar de homepage</a>
+  </div>
+  <!-- end .page-heading -->
+
+  <div class="main main--white">
+
+    <main class="main__column main__column--body text-holder">
+
+      <h1>Deze pagina is niet gevonden</h1>
+      <h5>Sorry! De pagina die je zoekt bestaat niet of niet meer.</h5>
+
+    </main>
+    <!-- end .main__column -->
+
+    <aside class="main__column main__column--aside main__column--empty sidebar">
+
+    </aside>
+    <!-- end .main__column -->
+
+  </div>
+  <!-- end .main -->
+
+<?php get_footer(); ?>
