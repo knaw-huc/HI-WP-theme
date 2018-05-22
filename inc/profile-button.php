332fb46935dd344ff51860dfa3a93de0249729c5
Pieter de Jong <contact@pieter-de-jong.nl>
2018-05-22 20:18:03 +0200

    Make profile image optional

--- inc/profile-button.php
+++ inc/profile-button.php
@@ -1,7 +1,13 @@
 <a href="<?php the_permalink(); ?>" class="profile-button">
 
   <div class="profile-button__portrait image-filter">
-    <?php echo wp_get_attachment_image(get_field('profile_image')['id'], 'profile-button--regular', 0, array('alt' => get_the_title())); ?>
+
+    <?php if(get_field('profile_image')) { ?>
+      <?php echo wp_get_attachment_image(get_field('profile_image')['id'], 'profile-button--regular', 0, array('alt' => get_the_title())); ?>
+    <?php } else { ?>
+        <img width="160" height="160" src="<?php bloginfo('template_url'); ?>/img/profile-anonymous.png" class="attachment-profile-button--regular size-profile-button--regular" alt="<?= get_the_title(); ?>" />
+    <?php } ?>
+
   </div>
   <!-- end .profile-button__portrait -->
 
