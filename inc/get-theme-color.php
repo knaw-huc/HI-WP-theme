2fd9e0ce1d19dbfcb86873075ce4399dfa5b68eb
Pieter de Jong <contact@pieter-de-jong.nl>
2022-07-05 11:32:11 +0200

    ✨ Update theme colors

--- inc/get-theme-color.php
+++ inc/get-theme-color.php
@@ -1,13 +1,13 @@
 <?php
 
 if($thema->slug == 'politieke-cultuur-en-geschiedenis') {
-  $themaColor = 'orange';
-} else if($thema->slug == 'kennis-en-kunstpraktijken') {
-  $themaColor = 'green';
-} else if($thema->slug == 'liveslab') {
   $themaColor = 'yellow';
-} else if($thema->slug == 'digitale-edities') {
+} else if($thema->slug == 'kennis-en-kunstpraktijken') {
+  $themaColor = 'orange';
+} else if($thema->slug == 'liveslab') {
   $themaColor = 'blue';
+} else if($thema->slug == 'digitale-edities') {
+  $themaColor = 'green';
 } else if($thema->slug == 'computationele-literatuurwetenschap') {
   $themaColor = 'purple';
 } else if($thema->slug == 'datamanagement') {
